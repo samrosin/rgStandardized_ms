3e68448b0cd99c4e38acb92a9aa5ec3af66b2f55
Samuel Rosin <samuelrosin@gmail.com>
2021-02-24 18:50:25 -0500

    initial commit, move sim data generation functions into separate file

--- estimation_fns.R
+++ estimation_fns.R
@@ -82,21 +82,3 @@ ests_std <- function(sample, sigma_e_hat, sigma_p_hat, n_1, n_2, n_3, vars_std, 
   c(pi_hat_st, var_pi_hat_st)
 }
 
-gen_data_scenario1 <- function(n_1, sigma_e, n_2, sigma_p, n_3, prev){
-  #### (1) Run the two validation studies
-  sigma_e_hat <- rbinom(1,n_1,sigma_e)/n_1 #estimated sensitivity
-  sigma_p_hat <-1-rbinom(1,n_2,1-sigma_p)/n_2 #estimated specificity
-  
-  ### (2) Generate the sample from the population based on Y, sigma_e, sigma_p, and prev 
-  y <- rbinom(n = n_3, size = 1, prob = prev)
-  sample <- data.frame(y, x=NA)
-  sample[sample$y==1,]$x <- as.integer(
-    rbinom(n = nrow(sample[sample$y==1,]), size = 1, sigma_e)) #positives test positive with probability sigma_e
-  sample[sample$y==0,]$x <- as.integer(
-    rbinom(n = nrow(sample[sample$y==0,]), size = 1, 1-sigma_p) #negatives test positive with probability 1-sigma_p
-  )
-  
-  rho_hat <- mean(sample$x) 
-  data <- list(sigma_e_hat = sigma_e_hat, sigma_p_hat = sigma_p_hat, rho_hat = rho_hat)
-  data
-}
