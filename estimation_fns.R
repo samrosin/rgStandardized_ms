dfe800b5af8fe02facbb7218d433fa38d608a2c1
Samuel Rosin <samuelrosin@gmail.com>
2021-02-24 20:04:21 -0500

    return number of strata when computing standardization estimates

--- estimation_fns.R
+++ estimation_fns.R
@@ -65,7 +65,7 @@ ests_std <- function(sample, sigma_e_hat, sigma_p_hat, n_1, n_2, n_3, vars_std, 
   pi_hat_st <- (sum(strata$std_est) - (1-sigma_p_hat))/(sigma_e_hat - (1-sigma_p_hat)) #point estimate
   
   if(variance == TRUE){
-    return(pi_hat_st)
+    return(c(pi_hat_st, nrow(strata))) #include number of strata in return vector
   } else{ 
     # compute variance estimator by summing the three components. compare to formula in manuscript. 
     a <- pi_hat_std^2 * sigma_e_hat * (1 - sigma_e_hat) / n_1
@@ -79,6 +79,6 @@ ests_std <- function(sample, sigma_e_hat, sigma_p_hat, n_1, n_2, n_3, vars_std, 
   }
   
   #return point and variance estimates
-  c(pi_hat_st, var_pi_hat_st)
+  c(pi_hat_st, var_pi_hat_st, nrow(strata)) #include number of strata in return vector
 }
 
