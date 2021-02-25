3fe568e44f789db6600f9916e042ae676e1e2bba
Samuel Rosin <samuelrosin@gmail.com>
2021-02-25 11:25:34 -0500

    add logistic regression model-based estimation

--- estimation_fns.R
+++ estimation_fns.R
@@ -31,7 +31,7 @@ ests_rg <- function(rho_hat, sigma_e_hat, sigma_p_hat, n_1, n_2, n_3, variance =
   }
 }
 
-#' Compute a point estimate of prevalence using standardization and a Rogan-Gladen adjustment. 
+#' Compute prevalence estimates using standardization and a Rogan-Gladen adjustment. 
 #' Corresponds to \hat \pi_{st} in the manuscript. 
 #'
 #' @param sample A dataframe with data from the sample, including the stratum_props (gamma_js) which will be used in standardization
@@ -39,13 +39,14 @@ ests_rg <- function(rho_hat, sigma_e_hat, sigma_p_hat, n_1, n_2, n_3, variance =
 #' @param sigma_p_hat Estimated specificity
 #' @param n_1 Sample size for sensitivity validation dataset
 #' @param n_2 Sample size for specificity validation dataset
+#' @param n_3 Sample size for main study from population
 #' @param vars_std The names of variables to standardize over, corresponding to columns in sample
 #' @param variance Should variance be estimated?
 #'
 #' @return Estimated prevalence; optionally, a vector also including variance estimate
 #' @export
 #'
-#' @examples point_est_std(sample, sigma_e_hat, sigma_p_hat, n_1, n_2, vars_std)
+#' @examples ests_std(sample, sigma_e_hat, sigma_p_hat, n_1, n_2, n_3, vars_std, variance = TRUE)
 ests_std <- function(sample, sigma_e_hat, sigma_p_hat, n_1, n_2, n_3, vars_std, variance = FALSE){
   #unique covariate strata in the sample data
   strata <- unique(sample[,c(vars_std,"stratum_prop")])
@@ -79,7 +80,114 @@ ests_std <- function(sample, sigma_e_hat, sigma_p_hat, n_1, n_2, n_3, vars_std, 
   }
   
   #return point and variance estimates
-  c(pi_hat_st, var_pi_hat_st, nrow(strata)) #include number of strata in return vector
+  return(c(pi_hat_st, var_pi_hat_st, nrow(strata))) #include number of strata in return vector
 }
+
+#' Compute prevalence estimates using Rogan-Gladen adjustment and
+#' standardization with an logistic regression outcome model for the mismeasured 
+#' stratum-specific prevalences rho_j.
+#' Corresponds to \hat \pi_{mst} in the manuscript. 
+#'
+#' @param sample A dataframe with data from the sample, including the stratum_props (gamma_js) which will be used in standardization
+#' @param sigma_e_hat Estimated sensitivity 
+#' @param sigma_p_hat Estimated specificity
+#' @param n_1 Sample size for sensitivity validation dataset
+#' @param n_2 Sample size for specificity validation dataset
+#' @param n_3 Sample size for main study from population
+#' @param vars_std The names of variables to standardize over, corresponding to columns in sample
+#' @param mod_formula An object of class "formula" that symbolically describes the model to be fitted
+#' @param variance Should variance be estimated?
+#'
+#' @return Estimated prevalence; optionally, a vector also including variance estimate
+#' @export
+#'
+#' @examples ests_std(sample, sigma_e_hat, sigma_p_hat, n_1, n_2, n_3, vars_std, 
+#'                    mod_formula = formula("x ~ z1 + z2 + z3"), variance = TRUE)
+
+ests_std_model <- function(sample, stratum_props, sigma_e_hat, 
+                           sigma_p_hat, n_1, n_2, n_3, vars_std,
+                           mod_formula, variance = FALSE){
+  
+  #fit the logistic regression
+  x_model <- glm(formula = mod_formula, family = binomial, data = sample) #regression model
+  
+  # make predictions for all strata
+  stratum_props$model_pred <- predict(x_model, newdata = stratum_props, type = "response")
+  stratum_props$std_est_model <- stratum_props$model_pred * stratum_props$stratum_prop
+  
+  # point estimate
+  pi_hat_mst <- (sum(stratum_props$std_est_model) - (1 - sigma_p_hat)) / 
+    (sigma_e_hat - (1 - sigma_p_hat))
+  
+  if(variance == FALSE){
+    return(pi_hat_mst)
+  } else{
+    
+    n <- n_1 + n_2 + n_3 #total sample size
+    
+    ##### 
+    # estimate the different parts of the variance estimator
+    coef <- as.matrix(coef(x_model)) # regression coefficients 
+    mat <- model.matrix(x_model) # design matrix
+    a <- matrix(c(n_1 / n, 0, 0, n_2 / n), nrow = 2, ncol = 2) # estimate A
+    
+    # estimate B
+    b <- matrix(NA, nrow = nrow(coef), ncol = nrow(coef)) # declare B-hat as an empty p * p matrix 
+    for(j in 1:nrow(coef)){
+      for(k in 1:nrow(coef)){
+        unit_contrib_b <- rep(NA, nrow(mat)) # each unit's contribution to this jkth element of B
+        for(i in 1:nrow(mat)){
+          unit_contrib_b[i] <- mat[i,j] * mat[i,k] * exp(t(coef) %*% as.matrix(mat[i,])) / 
+            (1 + exp(t(coef) %*% as.matrix(mat[i,])))^2
+        }
+        b[j,k] <- (n_3 / n) * mean(unit_contrib_b)
+      }
+    }
+    
+    c <- matrix(c(0, pi_hat_mst, 0, -1 + pi_hat_mst), nrow = 2, ncol = 2) # estimate C
+    
+    # estimate D
+    #model.matrix(~ strata$z1 + strata$z2 + strata$z3)
+    
+    #unique covariate strata in the sample data
+    strata <- unique(sample[,c(vars_std,"stratum_prop")])
+    mat_strat <- model.matrix(~ strata$z1 + strata$z2 + strata$z3) #model matrix for *all* strata, even those not in sample support
+    d <- matrix(0, nrow = 2, ncol = nrow(coef)) # declare D as 2 * p matrix of 0s (convenient since the 2nd row is 0)
+    for(l in 1:nrow(coef)){
+      stratum_contrib <- rep(NA, nrow(mat_strat)) # contribution of the jth stratum to the sum
+      for(j in 1:nrow(mat_strat)){
+        stratum_contrib[j] <- as.matrix(mat_strat[j,l]) * exp(t(coef) %*% as.matrix(mat_strat[j,])) /
+          (1 + exp(t(coef) %*% as.matrix(mat_strat[j,])))^2 * strata$stratum_prop[j]
+      }
+      d[1,l] <- -1 * sum(stratum_contrib)
+    }
+    
+    e <- matrix(c(1, -1, 0, sigma_e_hat + sigma_p_hat -1), nrow = 2, ncol = 2) # estimate E
+    f <- matrix(c(sigma_e_hat * (1 - sigma_e_hat), 0,
+                  0, sigma_p_hat * (1 - sigma_p_hat)), nrow = 2, ncol = 2) # estimate F 
+    
+    # estimate G 
+    g <- matrix(NA, nrow = nrow(coef), ncol = nrow(coef)) #declare G as an empty p * p matrix 
+    for(j in 1:nrow(coef)){
+      for(k in 1:nrow(coef)){
+        unit_contrib_g <- rep(NA, nrow(mat)) # each unit's contribution to this jkth element of B
+        for(i in 1:nrow(mat)){
+          unit_contrib_g[i] <- mat[i,j] * mat[i,k] * 
+            (x_model$y[i] - inv.logit(t(coef) %*% as.matrix(mat[i,])))^2 #note that x_model$y is the model outcome, which is in our model denoted x
+        }
+        g[j,k] <- (n_3 / n) * mean(unit_contrib_g)
+      }
+    }
+    
+    pt1 <- solve(e) %*% c %*% solve(a) %*% f %*% solve(a) %*% t(c) %*% t(solve(e)) 
+    pt2 <- solve(e) %*% d %*% solve(b) %*% g %*% solve(b) %*% t(d) %*% t(solve(e))
+    final <- pt1 + pt2
+    var_hat_pi_mst <- final[2,2]/n # the lower-right element divided by n is the variance estimator of interest
+    
+    c(pi_hat_mst, var_hat_pi_mst, nrow(strata))
+  }
+  
+}
+
 
 
