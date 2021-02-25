d5281aee1da6297932e64fce5a0fd760d6b34d3e
Samuel Rosin <samuelrosin@gmail.com>
2021-02-24 21:09:19 -0500

    move and rename some files

--- sims/input_files/sim_param_values.R
+++ sims/input_files/sim_param_values.R
@@ -3,7 +3,10 @@
 
 set.seed(2021)
 sigma_e <- c(.7,.95,1) #true sensitivity
+#sigma_p <- .95
+n_1 <- c(30,300)
+n_2 <- c(30,300)
 sigma_p <- c(.7,.95,1) #true specificity
-n_1 <- c(30,300,3000) #sensitivity study sample size
-n_2 <- c(30,300,3000) #specificity study sample size
+#n_1 <- c(30,300,3000) #sensitivity study sample size
+#n_2 <- c(30,300,3000) #specificity study sample size
 n_3 <- c(500,5000,20000) #main study sample size 
