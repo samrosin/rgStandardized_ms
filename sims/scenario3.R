ebc5e3cc57abaac8cfd3cf06cf1ce8f1922ee78d
Samuel Rosin <samuelrosin@gmail.com>
2021-02-25 11:26:41 -0500

    implement model-based estimates, and specify column types for input file

--- sims/scenario3.R
+++ sims/scenario3.R
@@ -12,11 +12,18 @@ output_file <- here("sims/results/scenario3_results.csv")
 
 #### The known stratum proportions (the gamma_{zj}s) must be prespecified,
 #### and they are loaded here
-gammas <- read_csv(here("sims/input_files/scenario3_stratum_props.csv"))
+gammas <- read_csv(here("sims/input_files/scenario3_stratum_props.csv"),
+                   col_types = cols(
+                     z1 = col_character(), 
+                     z2 = col_character(), 
+                     z3 = col_character(), 
+                     stratum_prop = col_double(),
+                     sampling_prob = col_double()
+                   ))
 
 # sim parameter values
 set.seed(2021)
-n_sims <- 2 # number of simulations
+n_sims <- 5 # number of simulations
 n_strata <- 40 # number of strata for this scenario
 vars_std <- c("z1", "z2", "z3")
 
@@ -55,18 +62,21 @@ sim_conditions <- tidyr::crossing(
   mutate(prev = sum(stratum_props$stratum_prop * stratum_props$prev),
          hat_pi = NA_real_,
          hat_pi_st = NA_real_, 
+         hat_pi_mst = NA_real_,
          num_infinite_pi = NA_real_, # number of infinite estimates \hat \pi
          num_infinite_pi_st = NA_real_, # number of infinite estimates \hat \pi_st
+         num_infinite_pi_mst = NA_real_, # number of infinite estimates \hat \pi_mst
          n_strata_obs_full = NA_real_ # number of simulations with positivity (all strata observed)
   )
 
 
 # conduct the simulation, iterating through the subscenarios
 for(i in 1:nrow(sim_conditions)){
-  print(i)
+  #print(i)
   row <- sim_conditions[i,]
   hat_pi <- rep(NA, n_sims)
   hat_pi_st <- rep(NA, n_sims)
+  hat_pi_mst <- rep(NA, n_sims)
   strata_obs <- rep(NA, n_sims) # number of observed strata in a sim
   
   # iterate through each of the n_sims simulations per sub-scenario
@@ -78,17 +88,30 @@ for(i in 1:nrow(sim_conditions)){
     hat_pi_st_vec <- ests_std(dat$sample, dat$sigma_e_hat, dat$sigma_p_hat, 
                               row$n_1, row$n_2, row$n_3, vars_std, variance = FALSE)
     hat_pi_st[j] <- hat_pi_st_vec[1]
-    strata_obs[j] <- hat_pi_st_vec[2]
+    strata_obs[j] <- hat_pi_st_vec[2] # Note we can get this info from either standardization estimator,
+                                      # hat_pi_st or hat_pi_mst; here I take it from hat_pi_st
+    hat_pi_mst[j] <- ests_std_model(
+              dat$sample, as.data.frame(row$stratum_props), dat$sigma_e_hat, 
+              dat$sigma_p_hat, row$n_1, row$n_2, row$n_3, vars_std, 
+              mod_formula = formula("x ~ z1 + z2 + z3"), variance = FALSE
+    )
+    
   }
   
   # compute mean relative bias of the finite estimates for the sub-scenario,
-  # and compute the other data of interest
+  # and compute the other results of interest
   sim_conditions[i,"hat_pi"] <- 100 * ( 
     mean(hat_pi[is.finite(hat_pi)]) - row$prev ) / row$prev 
   sim_conditions[i,"num_infinite_pi"] <- sum(!is.finite(hat_pi))
+  
   sim_conditions[i,"hat_pi_st"] <- 100 * (
     mean(hat_pi_st[is.finite(hat_pi_st)]) - row$prev) / row$prev 
   sim_conditions[i,"num_infinite_pi_st"] <- sum(!is.finite(hat_pi_st))
+  
+  sim_conditions[i,"hat_pi_mst"] <- 100 * (
+    mean(hat_pi_mst[is.finite(hat_pi_mst)]) - row$prev) / row$prev 
+  sim_conditions[i,"num_infinite_pi_mst"] <- sum(!is.finite(hat_pi_mst))
+  
   sim_conditions[i,"n_strata_obs_full"] <- sum(strata_obs == n_strata)
 }
 
