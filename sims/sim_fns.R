c9fa5c79e40f5145a1797c0955b7301ece565b2d
Samuel Rosin <samuelrosin@gmail.com>
2021-02-24 19:58:58 -0500

    add gen data function for scenario 2

--- sims/sim_fns.R
+++ sims/sim_fns.R
@@ -1,20 +1,60 @@
-# Functions helpful for conducted the simulation study. 
+# Functions helpful for conducting the simulation study. 
 
+# based on true parameters, generates data and returns data neeeded for analysis
 gen_data_scenario1 <- function(n_1, sigma_e, n_2, sigma_p, n_3, prev){
-  #### (1) Run the two validation studies
-  sigma_e_hat <- rbinom(1,n_1,sigma_e)/n_1 #estimated sensitivity
-  sigma_p_hat <-1-rbinom(1,n_2,1-sigma_p)/n_2 #estimated specificity
   
-  ### (2) Generate the sample from the population based on Y, sigma_e, sigma_p, and prev 
+  # (1) Run the two validation studies
+  sigma_e_hat <- rbinom(1 , n_1, sigma_e) / n_1 #estimated sensitivity
+  sigma_p_hat <-1 - rbinom(1, n_2, 1 - sigma_p) / n_2 #estimated specificity
+  
+ 
+  # (2) Generate a sample of size n_3 where each person's serostatus is distributed Bern(\pi)
   y <- rbinom(n = n_3, size = 1, prob = prev)
-  sample <- data.frame(y, x=NA)
-  sample[sample$y==1,]$x <- as.integer(
-    rbinom(n = nrow(sample[sample$y==1,]), size = 1, sigma_e)) #positives test positive with probability sigma_e
-  sample[sample$y==0,]$x <- as.integer(
-    rbinom(n = nrow(sample[sample$y==0,]), size = 1, 1-sigma_p) #negatives test positive with probability 1-sigma_p
-  )
   
+  # (3) Generate test results x based on Y, sigma_e, and sigma_p
+  sample <- data.frame(y, x = NA)
+  sample[sample$y==1,]$x <- as.integer(
+    rbinom(n = nrow(sample[sample$y==1,]), size = 1, sigma_e)
+    ) #positives test positive with probability sigma_e
+  sample[sample$y==0,]$x <- as.integer(
+    rbinom(n = nrow(sample[sample$y==0,]), size = 1, 1 - sigma_p)
+  )  #negatives test positive with probability 1 - sigma_p
+  
+  # (4) Return sample values 
   rho_hat <- mean(sample$x) 
-  data <- list(sigma_e_hat = sigma_e_hat, sigma_p_hat = sigma_p_hat, rho_hat = rho_hat)
+  data <- list(sigma_e_hat = sigma_e_hat, 
+               sigma_p_hat = sigma_p_hat, 
+               rho_hat = rho_hat)
+  data
+}
+
+# based on true parameters, generates data and returns data neeeded for analysis
+gen_data_scenario2 <- function(n_1, sigma_e, n_2, sigma_p, n_3, stratum_props){
+  
+  # (1) Run the two validation studies
+  sigma_e_hat <- rbinom(1 , n_1, sigma_e) / n_1 #estimated sensitivity
+  sigma_p_hat <-1 - rbinom(1, n_2, 1 - sigma_p) / n_2 #estimated specificity
+  
+  # (2) Create main study sample based on sampling probs s_js 
+  sample <- stratum_props[sample(
+    nrow(stratum_props), n_3, replace = T, prob = stratum_props$sampling_prob),]
+  
+  # (3) Each person's true serology status is distributed Bernoulli
+  #     with a stratum-specific mean (according to the vector sample$prev)
+  sample$y <- rbinom(n = n_3, size = 1, prob = sample$prev)
+  
+  # (4) The test results are generated based on Y, sigma_e, and sigma_p
+  sample$x <- NA
+  sample[sample$y==1,]$x <- as.integer(
+    rbinom(n = nrow(sample[sample$y==1,]), size = 1, sigma_e)
+    ) #positives test positive with probability sigma_e
+  sample[sample$y==0,]$x <- as.integer(
+    rbinom(n = nrow(sample[sample$y==0,]), size = 1, 1 - sigma_p) 
+    ) #negatives test positive with probability 1 - sigma_p
+  
+  # (5) Return sample values
+  rho_hat <- mean(sample$x) 
+  data <- list(sigma_e_hat = sigma_e_hat, sigma_p_hat = sigma_p_hat, 
+               rho_hat = rho_hat, sample = sample)
   data
 }
