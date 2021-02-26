adf713e3c24d8e2eb3ad1cae0019ca3b26a36207
Samuel Rosin <samuelrosin@gmail.com>
2021-02-26 09:41:11 -0500

    create results_draft and results_final subdirectory structure

--- sims/input_files/sim_param_values.R
+++ sims/input_files/sim_param_values.R
@@ -2,13 +2,15 @@
 # If desired, these parameter values could vary across scenarios. 
 
 set.seed(2021)
-sigma_e <- c(.7,.95,1) #true sensitivity
+#sigma_e <- c(.95) #true sensitivity
+sigma_e <- c(.7, .95, 1)
 #sigma_p <- .95
-n_1 <- c(30,300)
-n_2 <- c(30,300)
-sigma_p <- c(.7,.95,1) #true specificity
-#n_1 <- c(30,300,3000) #sensitivity study sample size
-#n_2 <- c(30,300,3000) #specificity study sample size
+sigma_p <- c(.7, .95, 1)
+#n_1 <- c(30,300)
+#n_2 <- c(30,300)
+#n_3 <- c(500,5000) #main study sample size 
+n_1 <- c(30,300,3000) #sensitivity study sample size
+n_2 <- c(30,300,3000) #specificity study sample size
 n_3 <- c(500,5000,20000) #main study sample size 
 
 # Parameters for the logistic model that determines 
--- sims/scenario1.R
+++ sims/scenario1.R
@@ -1,5 +1,6 @@
 ##########
 # Conducts simulation scenario 1 
+time1 <- Sys.time() 
 
 library(tidyverse)
 library(looplot) #install with devtools::install_github("matherealize/looplot")
@@ -7,11 +8,16 @@ library(here)
 
 source(here("estimation_fns.R"))
 source(here("sims/input_files/sim_param_values.R")) #load sim parameter values common across scenarios
-output_file <- here("sims/results/scenario1_results.csv")
+source(here("sims/sim_fns.R"))
+
+# Note that the final simulation results are placed in the 
+# 'results_final' subdirectory, but as the simulations are conducted
+# results are placed in the results_draft subdirectory
+output_file <- here("sims/results_draft/scenario1_results.csv")
 
 # sim parameter values
 set.seed(2021)
-n_sims <- 5 #number of simulations
+n_sims <- 100 #number of simulations
 prev <- c(.005,.05,.3) #true prevalence 
 
 # fully factorial combination of sample sizes and parameters
@@ -40,3 +46,5 @@ for(i in 1:nrow(sim_conditions)){
 # since the above simulation can take some time, 
 # write results to an output file to analyse in a separate script
 write_csv(sim_conditions, output_file)
+time2 <- Sys.time()
+print(time2 - time1)
