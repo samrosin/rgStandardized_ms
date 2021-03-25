c0e757a167ea8aa8d3be5157c7121132fa229ed8
Samuel Rosin <samuelrosin@gmail.com>
2021-03-25 12:40:22 -0400

    add DGPs 2, 3, 4

--- sims/finalize_results.R
+++ sims/finalize_results.R
@@ -9,3 +9,26 @@ results_1 <- read_csv(here("sims/results_draft/scenario1_results.csv"),
                     pi = prev)
 write_csv(results_1, here("sims/results_final/scenario1_results.csv"))
 
+results_2 <- read_csv(here("sims/results_draft/scenario2_results.csv"),
+                      col_types = cols(.default = col_double())) %>% 
+             rename(hat_pi_RG = hat_pi,
+                     hat_pi_SRG = hat_pi_st,
+                     pi = prev)
+write_csv(results_2, here("sims/results_final/scenario2_results.csv"))
+
+results_3 <- read_csv(here("sims/results_draft/scenario3_results.csv"),
+                      col_types = cols(.default = col_double())) %>% 
+  rename(hat_pi_RG = hat_pi,
+         hat_pi_SRG = hat_pi_st,
+         hat_pi_SRGM = hat_pi_mst,
+         pi = prev)
+write_csv(results_3, here("sims/results_final/scenario3_results.csv"))
+
+results_4 <- read_csv(here("sims/results_draft/scenario4_results.csv"),
+                      col_types = cols(.default = col_double())) %>% 
+  rename(hat_pi_RG = hat_pi,
+         hat_pi_SRG = hat_pi_st,
+         hat_pi_SRGM = hat_pi_mst,
+         pi = prev)
+write_csv(results_4, here("sims/results_final/scenario4_results.csv"))
+
