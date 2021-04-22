a55acf33146ea35364fb11e9ccb67eb349ba42e8
Samuel Rosin <samuelrosin@gmail.com>
2021-04-22 13:56:01 -0400

    add SRG-restriction estimator and store sims with nonpositivity

--- sims/bias_sims/scenario4_bias.R
+++ sims/bias_sims/scenario4_bias.R
@@ -17,7 +17,7 @@ output_file <- here("sims/results_draft/scenario4_results.csv")
 
 #### The known stratum proportions (the gamma_{zj}s) must be prespecified,
 #### and they are loaded here
-gammas <- read_csv(here("sims/input_files/scenario4_stratum_props.csv"),
+gammas <- read_csv(here("sims/input_files/scenario4_stratum_props_ALT.csv"),
                    col_types = cols(
                      z1 = col_character(), 
                      z2 = col_character(), 
@@ -29,14 +29,14 @@ gammas <- read_csv(here("sims/input_files/scenario4_stratum_props.csv"),
 
 # sim parameter values
 set.seed(2021)
-n_sims <- 5 # number of simulations
+n_sims <- 1000 # number of simulations
 n_strata <- 80 # number of strata for this scenario
 vars_std <- c("z1", "z2", "z3", "z4")
 
 # Create copies of the gamma (stratum_prop) dataframe,
 # with stratum-specific prevalence created from a true logistic model. 
 # The intercept of the logistic model varies to vary the marginal prevalence
-prevs <- seq(.01, .2, by = .01)
+prevs <- seq(.01, .05, by = .01)
 stratum_props <- vector(mode = "list", length = length(prevs)) # create list of stratum proportion dataframes
 for(p in 1:length(prevs)){
   s <- gammas %>% dplyr::mutate(
@@ -68,7 +68,7 @@ sim_conditions <- tidyr::crossing(
          num_infinite_pi_RG = NA_real_, # number of infinite estimates \hat \pi_RG
          num_infinite_pi_SRG = NA_real_, # number of infinite estimates \hat \pi_SRG
          num_infinite_pi_SRGM = NA_real_, # number of infinite estimates \hat \pi_SRGM
-         n_strata_obs_full = NA_real_ # number of simulations with positivity (all strata observed)
+         sims_w_positivity = NA_real_ # number of simulations with positivity (all strata observed)
   )
 
 # conduct the simulation, iterating through the subscenarios
@@ -77,20 +77,24 @@ for(i in 1:nrow(sim_conditions)){
   row <- sim_conditions[i,]
   hat_pi_RG <- rep(NA, n_sims)
   hat_pi_SRG <- rep(NA, n_sims)
+  hat_pi_SRG_restriction <- rep(NA, n_sims)
   hat_pi_SRGM <- rep(NA, n_sims)
   strata_obs <- rep(NA, n_sims) # number of observed strata in a sim
+  positivity <- rep(NA, n_sims) # is there positivity? True/False
   
   # iterate through each of the n_sims simulations per sub-scenario
   for(j in 1:n_sims){
     dat <- gen_data_scenario3(row$n_1, row$sigma_e, row$n_2, row$sigma_p, 
                               row$n_3, as.data.frame(row$stratum_props), vars_std)
     hat_pi_RG[j] <- ests_rg(dat$rho_hat, dat$sigma_e_hat, dat$sigma_p_hat, 
-                         row$n_1, row$n_2, row$n_3, variance = FALSE)
+                            row$n_1, row$n_2, row$n_3, variance = FALSE)
     hat_pi_SRG_vec <- ests_std(dat$sample, dat$sigma_e_hat, dat$sigma_p_hat, 
-                              row$n_1, row$n_2, row$n_3, vars_std, variance = FALSE)
+                               row$n_1, row$n_2, row$n_3, vars_std, variance = FALSE)
     hat_pi_SRG[j] <- hat_pi_SRG_vec[1]
+    
     strata_obs[j] <- hat_pi_SRG_vec[2] # Note we can get this info from either standardization estimator,
     # hat_pi_SRG or hat_pi_SRGM; here I take it from hat_pi_SRG
+    positivity[j] <- ifelse(hat_pi_SRG_vec[2] < n_strata, FALSE, TRUE)
     
     hat_pi_SRGM[j] <- ests_std_model(
       dat$sample, as.data.frame(row$stratum_props), dat$sigma_e_hat,
@@ -108,23 +112,27 @@ for(i in 1:nrow(sim_conditions)){
   sim_conditions[i,"num_infinite_pi_RG"] <- sum(!is.finite(hat_pi_RG))
   
   sim_conditions[i,"hat_pi_SRG"] <- 100 * (
-    mean(hat_pi_SRG[is.finite(hat_pi_SRG)]) - row$prev) / row$prev 
+    mean(hat_pi_SRG[positivity], na.rm = TRUE) - row$prev) / row$prev 
+  sim_conditions[i,"num_infinite_pi_SRG"] <- sum(!is.finite(hat_pi_SRG))
+  
+  sim_conditions[i,"hat_pi_SRG_restriction"] <- 100 * (
+    mean(hat_pi_SRG[!positivity], na.rm = TRUE) - row$prev) / row$prev 
   sim_conditions[i,"num_infinite_pi_SRG"] <- sum(!is.finite(hat_pi_SRG))
   
   sim_conditions[i,"hat_pi_SRGM"] <- 100 * (
     mean(hat_pi_SRGM[is.finite(hat_pi_SRGM)]) - row$prev) / row$prev 
   sim_conditions[i,"num_infinite_pi_SRGM"] <- sum(!is.finite(hat_pi_SRGM))
   
-  sim_conditions[i,"n_strata_obs_full"] <- sum(strata_obs == n_strata)
+  sim_conditions[i,"sims_w_positivity"] <- sum(positivity)
 }
 
 # since the above simulation can take some time, 
 # write results to an output file to analyse in a separate script
 sim_results <- sim_conditions %>% dplyr::select(-c(stratum_props)) %>%
-                                  dplyr::rename(pi = prev)
+  dplyr::rename(pi = prev)
 sim_results$n_sims <- NA_real_
 sim_results[1,"n_sims"] <- n_sims # store number of simulations (in a non-tidy format, 
-                                  # but it is useful to store this value somewhere and without repeating it in each row)
+# but it is useful to store this value somewhere and without repeating it in each row)
 #write_csv(sim_results, output_file)
 time2 <- Sys.time()
 print(time2 - time1)
