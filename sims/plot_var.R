93d51514600e15267ea54c32cce7cca9edc0ebdb
Samuel Rosin <samuelrosin@gmail.com>
2021-09-22 21:08:52 -0400

    plots for dgps 3-6

--- sims/plot_var.R
+++ sims/plot_var.R
@@ -21,9 +21,9 @@ results_2 <- read_csv(here("sims/results_final/dgp2_var_results.csv"),
                       col_types = cols(.default = col_double())
 )
 
-# results_3 <- read_csv(here("sims/results_final/dgp3_var_results.csv"),
-#                       col_types = cols(.default = col_double())
-# )
+results_3 <- read_csv(here("sims/results_final/dgp3_var_results.csv"),
+                      col_types = cols(.default = col_double())
+)
 gammas_3 <- read_csv(here("sims/inputs/dgp3_stratum_props.csv"),
                      col_types = cols(
                        z1 = col_character(),
@@ -33,9 +33,9 @@ gammas_3 <- read_csv(here("sims/inputs/dgp3_stratum_props.csv"),
                        sampling_prob = col_double()
                      ))
 
-# results_4 <- read_csv(here("sims/results_final/dgp4_var_results.csv"),
-#                       col_types = cols(.default = col_double())
-# )
+results_4 <- read_csv(here("sims/results_final/dgp4_var_results.csv"),
+                      col_types = cols(.default = col_double())
+)
 
 gammas_4 <- read_csv(here("sims/inputs/dgp4_stratum_props.csv"),
                      col_types = cols(
@@ -59,7 +59,7 @@ results_6 <- read_csv(here("sims/results_final/dgp6_var_results.csv"),
 
 gammas_6 <- gammas_4
 
-# DGP 1 Plots --------------------------------------------------------------
+# dgp 1 Plots --------------------------------------------------------------
 res1_gg <- results_1 %>% filter(n_1 == 40 & sigma_e != .6) %>% 
   dplyr::rename(Spec = sigma_p, Sens = sigma_e) %>% 
   gather(key = Method, value = covers,
@@ -68,39 +68,40 @@ res1_gg <- results_1 %>% filter(n_1 == 40 & sigma_e != .6) %>%
 
 res1_cov <- ggplot(data = res1_gg, 
                      mapping = aes(x = pi, y = covers,
-                                   linetype = Method, color = Method)) +
-  geom_line(size = 2) + 
+                                   linetype = Method, color = Method, size = Method)) +
+  geom_line() + 
   facet_grid(Sens ~ Spec, 
              labeller = labeller(.rows = label_both, .cols = label_both)) + 
-  theme_bw() + 
+  theme_classic() + 
   theme(axis.text.y = element_text(size = 16),
         axis.text.x = element_text(size = 16, angle = 90, hjust = 1, vjust = .5),
         axis.title = element_text(size = 20),
         legend.position = c(.93,0.58),
         legend.title = element_blank(),
         legend.text = element_text(size = 20),
-        #                    legend.background = element_rect(fill=alpha('white', 0)),
         panel.spacing.y = unit(1.25, "lines"),
         strip.text = element_text(size = 18)) + 
   labs(x = "Prevalence", y = "95% CI Coverage") + 
   scale_y_continuous(labels = function(x) paste0(x, "%"),
                      limits = c(80, 100)) + 
-  scale_linetype_manual(name = "Method", values = 2,
-                        labels = expression(hat(pi)[RG])) +
-  scale_color_manual(name = "Method", values = scales::hue_pal()(1),
-                     labels = expression(hat(pi)[RG])) + 
-  geom_hline(aes(yintercept = 95), linetype = "dashed") 
+  scale_linetype_manual(name = "Method", values = c("dashed"),
+                        labels = c(expression(hat(pi)[RG]))) + 
+  scale_color_manual(name = "Method", values = c("black"),
+                     labels = c(expression(hat(pi)[RG]))) + 
+  scale_size_manual(name = "Method", values = c(0.5),
+                    labels = c(expression(hat(pi)[RG]))) + 
+  geom_hline(aes(yintercept = 95), size = 0.5, linetype = "dotted", color = "gray") 
 
-res1_cov
+#res1_cov
 
 #send plot to pdf
-pdf(here("sims/figs/var/DGP1_coverage.pdf"),
+pdf(here("sims/figs/var/dgp1_coverage.pdf"),
     paper = "USr",width = 8.5, height = 11)
 print(res1_cov)
 dev.off()
 
-# DGP 2 Plots -------------------------------------------------------------
-res2_gg <- results_2 %>% filter(n_1 == 40 & sigma_e != .6) %>% 
+# dgp 2 Plots -------------------------------------------------------------
+res2_gg <- results_2 %>% 
   dplyr::rename(Spec = sigma_p, Sens = sigma_e) %>% 
   gather(key = Method, value = covers,
          covers_pi_RG, covers_pi_SRG) %>% 
@@ -108,34 +109,36 @@ res2_gg <- results_2 %>% filter(n_1 == 40 & sigma_e != .6) %>%
 
 res2_cov <- ggplot(data = res2_gg, 
                      mapping = aes(x = prev, y = covers,
-                                   linetype = Method, color = Method)) +
-  geom_line(size = 2) + 
+                                   linetype = Method, color = Method, size = Method)) +
+  geom_line() + 
   facet_grid(Sens ~ Spec, 
              labeller = labeller(.rows = label_both, .cols = label_both)) + 
-  theme_bw() + 
+  theme_classic() + 
   theme(axis.text.y = element_text(size = 16),
         axis.text.x = element_text(size = 16, angle = 90, hjust = 1, vjust = .5),
         axis.title = element_text(size = 20),
         legend.position = c(.92,0.6),
         legend.title = element_blank(),
         legend.text = element_text(size = 20),
-        #                    legend.background = element_rect(fill=alpha('white', 0)),
         panel.spacing.y = unit(1.25, "lines"),
         strip.text = element_text(size = 18)) + 
   labs(x = "Prevalence", y = "95% CI Coverage") + 
   scale_y_continuous(labels = function(x) paste0(x, "%"),
                      limits = c(80, 100)) + 
-  scale_linetype_manual(name = "Method", values = c(2, 1),
+  scale_linetype_manual(name = "Method", values = c("dashed", "solid"),
                         labels = c(expression(hat(pi)[RG]), 
                                    expression(hat(pi)[SRG]))) + 
-  scale_color_manual(name = "Method", values = scales::hue_pal()(3)[1:2],
+  scale_color_manual(name = "Method", values = c("black", "gray"),
                      labels = c(expression(hat(pi)[RG]), 
                                 expression(hat(pi)[SRG]))) + 
-  geom_hline(aes(yintercept = 95), linetype = "dashed") 
-res2_cov
+  scale_size_manual(name = "Method", values = c(0.5, 0.5),
+                    labels = c(expression(hat(pi)[RG]), 
+                               expression(hat(pi)[SRG]))) + 
+  geom_hline(aes(yintercept = 95), size = 0.5, linetype = "dotted", color = "gray") 
+#res2_cov
 
 #send plot to pdf
-pdf(here("sims/figs/var/DGP2_coverage.pdf"),
+pdf(here("sims/figs/var/dgp2_coverage.pdf"),
     paper = "USr",width = 8.5, height = 11)
 print(res2_cov)
 dev.off()
@@ -143,14 +146,106 @@ dev.off()
 
 # dgp 3 -------------------------------------------------------------------
 
+res3_gg <- results_3 %>% 
+  dplyr::rename(Spec = sigma_p, Sens = sigma_e) %>% 
+  gather(key = Method, value = covers,
+         coverage_pi_RG, 
+         coverage_pi_SRG, 
+         coverage_pi_SRGM) %>% 
+  mutate(covers = 100 * covers, 
+         Method = factor(Method, levels = unique(Method)))
+
+res3_cov <- ggplot(data = res3_gg, 
+                   mapping = aes(x = prev, y = covers,
+                                 linetype = Method, color = Method, size = Method)) +
+  geom_line() + 
+  facet_grid(Sens ~ Spec, 
+             labeller = labeller(.rows = label_both, .cols = label_both)) + 
+  theme_classic() + 
+  theme(axis.text.y = element_text(size = 16),
+        axis.text.x = element_text(size = 16, angle = 90, hjust = 1, vjust = .5),
+        axis.title = element_text(size = 20),
+        legend.position = c(.91,0.75),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 20),
+        strip.text = element_text(size = 18)) + 
+  labs(x = "Prevalence", y = "95% CI Coverage") + 
+  scale_y_continuous(labels = function(x) paste0(x, "%"),
+                     limits = c(0, 100)) + 
+  scale_linetype_manual(name = "Method", values = c("dashed", "solid", "solid"),
+                        labels = c(expression(hat(pi)[RG]), 
+                                   expression(hat(pi)[SRG]),
+                                   expression(hat(pi)[SRGM]))) + 
+  scale_color_manual(name = "Method", values = c("black", "gray", "black"),
+                     labels = c(expression(hat(pi)[RG]), 
+                                expression(hat(pi)[SRG]),
+                                expression(hat(pi)[SRGM]))) + 
+  scale_size_manual(name = "Method", values = c(0.5, 1.5, 0.5),
+                    labels = c(expression(hat(pi)[RG]), 
+                               expression(hat(pi)[SRG]),
+                               expression(hat(pi)[SRGM]))) + 
+  geom_hline(aes(yintercept = 95), size = 0.5, linetype = "dotted", color = "gray") 
+#res3_cov
+
+#send plot to pdf
+pdf(here("sims/figs/var/dgp3_coverage.pdf"),
+    paper = "USr",width = 8.5, height = 11)
+print(res3_cov)
+dev.off()
 
 # dgp 4 -------------------------------------------------------------------
 
+res4_gg <- results_4 %>% 
+  dplyr::rename(Spec = sigma_p, Sens = sigma_e) %>% 
+  gather(key = Method, value = covers,
+         coverage_pi_RG, 
+         coverage_pi_SRG, 
+         coverage_pi_SRGM) %>% 
+  mutate(covers = 100 * covers, 
+         Method = factor(Method, levels = unique(Method)))
+
+res4_cov <- ggplot(data = res4_gg, 
+                   mapping = aes(x = prev, y = covers,
+                                 linetype = Method, color = Method, size = Method)) +
+  geom_line() + 
+  facet_grid(Sens ~ Spec, 
+             labeller = labeller(.rows = label_both, .cols = label_both)) + 
+  theme_classic() + 
+  theme(axis.text.y = element_text(size = 16),
+        axis.text.x = element_text(size = 16, angle = 90, hjust = 1, vjust = .5),
+        axis.title = element_text(size = 20),
+        legend.position = c(.1,0.65),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 20),
+        strip.text = element_text(size = 18)) + 
+  labs(x = "Prevalence", y = "95% CI Coverage") + 
+  scale_y_continuous(labels = function(x) paste0(x, "%"),
+                    limits = c(0, 100)) + 
+  scale_linetype_manual(name = "Method", values = c("dashed", "solid", "solid"),
+                        labels = c(expression(hat(pi)[RG]), 
+                                   expression(hat(pi)[SRG]),
+                                   expression(hat(pi)[SRGM]))) + 
+  scale_color_manual(name = "Method", values = c("black", "gray", "black"),
+                     labels = c(expression(hat(pi)[RG]), 
+                                expression(hat(pi)[SRG]),
+                                expression(hat(pi)[SRGM]))) + 
+  scale_size_manual(name = "Method", values = c(0.5, 0.5, 0.5),
+                    labels = c(expression(hat(pi)[RG]), 
+                               expression(hat(pi)[SRG]),
+                               expression(hat(pi)[SRGM]))) + 
+  geom_hline(aes(yintercept = 95), size = 0.5, linetype = "dotted", color = "gray") 
+#res4_cov
+
+#send plot to pdf
+pdf(here("sims/figs/var/dgp4_coverage.pdf"),
+    paper = "USr",width = 8.5, height = 11)
+print(res4_cov)
+dev.off()
 
 
 # dgp 5 -------------------------------------------------------------
 
-# plot DGP5 for n_1 = 40
+# plot dgp5 for n_1 = 40
 
 res5_gg <- results_5 %>% filter(n_1 == 40 & sigma_e != .6) %>% 
   dplyr::rename(Spec = sigma_p, Sens = sigma_e) %>% 
@@ -163,44 +258,47 @@ res5_gg <- results_5 %>% filter(n_1 == 40 & sigma_e != .6) %>%
 
 res5_cov <- ggplot(data = res5_gg, 
                    mapping = aes(x = prev, y = covers,
-                                 linetype = Method, color = Method)) +
-  geom_line(size = 2) + 
+                                 linetype = Method, color = Method,
+                                 size = Method)) +
+  geom_line() + 
   facet_grid(Sens ~ Spec, 
              labeller = labeller(.rows = label_both, .cols = label_both)) + 
-  theme_bw() + 
+  theme_classic() + 
   theme(axis.text.y = element_text(size = 16),
         axis.text.x = element_text(size = 16, angle = 90, hjust = 1, vjust = .5),
         axis.title = element_text(size = 20),
         legend.position = c(.91,0.63),
         legend.title = element_blank(),
         legend.text = element_text(size = 20),
-        #                    legend.background = element_rect(fill=alpha('white', 0)),
-        panel.spacing.y = unit(1.25, "lines"),
         strip.text = element_text(size = 18)) + 
   labs(x = "Prevalence", y = "95% CI Coverage") + 
   scale_y_continuous(labels = function(x) paste0(x, "%"),
-                     limits = c(50, 100)) + 
-  scale_linetype_manual(name = "Method", values = c(2, 1, 3),
+                     limits = c(0, 100)) + 
+  scale_linetype_manual(name = "Method", values = c("dashed", "solid", "solid"),
                         labels = c(expression(hat(pi)[RG]), 
                                    expression(hat(pi)[SRG]),
                                    expression(hat(pi)[SRGM]))) + 
-  scale_color_manual(name = "Method", values = scales::hue_pal()(3),
+  scale_color_manual(name = "Method", values = c("black", "gray", "black"),
                      labels = c(expression(hat(pi)[RG]), 
                                 expression(hat(pi)[SRG]),
                                 expression(hat(pi)[SRGM]))) + 
-  geom_hline(aes(yintercept = 95), linetype = "dashed") 
-res5_cov
+  scale_size_manual(name = "Method", values = c(0.5, 0.5, 0.5),
+                     labels = c(expression(hat(pi)[RG]), 
+                                expression(hat(pi)[SRG]),
+                                expression(hat(pi)[SRGM]))) + 
+  geom_hline(aes(yintercept = 95), size = 0.5, linetype = "dotted", color = "gray")  
+#res5_cov
 
 #send plot to pdf
-pdf(here("sims/figs/var/DGP5_coverage.pdf"),
+pdf(here("sims/figs/var/dgp5_coverage.pdf"),
     paper = "USr",width = 8.5, height = 11)
 print(res5_cov)
 dev.off()
 
-# DGP 6 plots -------------------------------------------------------------
+# dgp 6 plots -------------------------------------------------------------
 
 # plot for n_1 == 40
-res6_gg <- results_6 %>% filter(n_1 == 40 & sigma_e != .6 ) %>% 
+res6_gg <- results_6 %>% 
   dplyr::rename(Spec = sigma_p, Sens = sigma_e) %>% 
   gather(key = Method, value = covers,
          coverage_pi_RG,
@@ -212,15 +310,15 @@ res6_gg <- results_6 %>% filter(n_1 == 40 & sigma_e != .6 ) %>%
 
 res6_cov <- ggplot(data = res6_gg, 
                    mapping = aes(x = prev, y = covers,
-                                 linetype = Method, color = Method)) +
-  geom_line(size = 2) + 
+                                 linetype = Method, color = Method, size = Method)) +
+  geom_line() + 
   facet_grid(Sens ~ Spec, 
              labeller = labeller(.rows = label_both, .cols = label_both)) + 
-  theme_bw() + 
+  theme_classic() + 
   theme(axis.text.y = element_text(size = 16),
         axis.text.x = element_text(size = 16, angle = 90, hjust = 1, vjust = .5),
         axis.title = element_text(size = 20),
-        legend.position = c(.91,0.63),
+        legend.position = c(.09,0.63),
         legend.title = element_blank(),
         legend.text = element_text(size = 20),
         #                    legend.background = element_rect(fill=alpha('white', 0)),
@@ -228,20 +326,25 @@ res6_cov <- ggplot(data = res6_gg,
         strip.text = element_text(size = 18)) + 
   labs(x = "Prevalence", y = "95% CI Coverage") + 
   scale_y_continuous(labels = function(x) paste0(x, "%"),
-                     limits = c(50, 100)) + 
-  scale_linetype_manual(name = "Method", values = c(2, 1, 3),
+                     limits = c(0, 100)) + 
+  scale_linetype_manual(name = "Method", values = c("dashed", "solid", "solid"),
                         labels = c(expression(hat(pi)[RG]), 
                                    expression(hat(pi)[SRG]),
                                    expression(hat(pi)[SRGM]))) + 
-  scale_color_manual(name = "Method", values = scales::hue_pal()(3),
+  scale_color_manual(name = "Method", values = c("black", "gray", "black"),
                      labels = c(expression(hat(pi)[RG]), 
                                 expression(hat(pi)[SRG]),
                                 expression(hat(pi)[SRGM]))) + 
-  geom_hline(aes(yintercept = 95), linetype = "dashed") 
-res6_cov
+  scale_size_manual(name = "Method", values = c(0.5, 0.5, 0.5),
+                    labels = c(expression(hat(pi)[RG]), 
+                               expression(hat(pi)[SRG]),
+                               expression(hat(pi)[SRGM]))) + 
+  geom_hline(aes(yintercept = 95), size = 0.5, linetype = "dotted", color = "gray")  
+#res6_cov
 
 #send plot to pdf
-pdf(here("sims/figs/var/DGP6_coverage.pdf"),
+pdf(here("sims/figs/var/dgp6_coverage.pdf"),
     paper = "USr",width = 8.5, height = 11)
 print(res6_cov)
 dev.off()
+
