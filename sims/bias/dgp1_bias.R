fb6ee1add447e39158ba46a59c101c8b416147b3
Samuel Rosin <samuelrosin@gmail.com>
2022-10-16 21:55:48 -0400

    rogan-gladen no-truncation estimator in dgp1 sims

--- sims/bias/dgp1_bias.R
+++ sims/bias/dgp1_bias.R
@@ -8,7 +8,6 @@ n_sims <- 1000 #number of simulations
 prev <- seq(.01, .20, by = .01) #true prevalence 
 
 library(tidyverse)
-library(looplot) #install with devtools::install_github("matherealize/looplot")
 library(here)
 
 source(here("estimation_fns.R"))
@@ -18,8 +17,8 @@ source(here("sims/sim_fns.R"))
 # Note that the final simulation results are placed in the 
 # 'results_final' subdirectory, but as the simulations are conducted
 # results are placed in the results_draft subdirectory
-output_file <- here("sims/results_draft/dgp1_results.csv")
-  
+output_file <- here("sims/results_draft/dgp1_notrunc_results.csv")
+
 # fully factorial combination of sample sizes and parameters
 # each row is a sub-scenario
 sim_conditions <- crossing(n_1, n_2, n_3, sigma_e, sigma_p, prev) %>% 
@@ -30,18 +29,23 @@ for(i in 1:nrow(sim_conditions)){
   print(i)
   row <- sim_conditions[i,]
   hat_pi_RG <- rep(NA, n_sims)
+  hat_pi_RG_notrunc <- rep(NA, n_sims)
   
   #iterate through each of the n_sims simulations per sub-scenario
   for(j in 1:n_sims){
     dat <- gen_data_dgp1(row$n_1, row$sigma_e, row$n_2, 
-             row$sigma_p, row$n_3, row$prev)
+                         row$sigma_p, row$n_3, row$prev)
     hat_pi_RG[j] <- ests_rg(dat$rho_hat, dat$sigma_e_hat, dat$sigma_p_hat, 
-                         row$n_1, row$n_2, row$n_3, variance = FALSE)
+                            row$n_1, row$n_2, row$n_3, variance = FALSE)
+    hat_pi_RG_notrunc[j] <- ests_rg_no_trunc(dat$rho_hat, dat$sigma_e_hat, dat$sigma_p_hat, 
+    row$n_1, row$n_2, row$n_3, variance = FALSE)
   }
   
   #compute mean relative bias of the finite estimates for the sub-scenario
   sim_conditions[i,"hat_pi_RG"] <- 100 * ( 
     mean(hat_pi_RG[is.finite(hat_pi_RG)]) - row$prev ) / row$prev 
+  sim_conditions[i,"hat_pi_RG_notrunc"] <- 100 * ( 
+    mean(hat_pi_RG_notrunc[is.finite(hat_pi_RG_notrunc)]) - row$prev ) / row$prev 
 }
 
 # write results to an output file 
